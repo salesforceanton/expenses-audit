ec39ff1f597376fd514dd72be8eb7009a7245eb8
Anton <antonlashkov@sumoscheduler.com>
2023-11-10 00:33:28 +0300

    create form input component to incapsulate some logic improve new cost form validation logic

--- src/components/newCostForm/NewCostForm.js
+++ src/components/newCostForm/NewCostForm.js
@@ -1,19 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState, useRef } from 'react';
+
 import Card from '../common/card/Card'
 import styles from './NewCostForm.module.css';
+import FormInput from '../common/formInput/FormInput';
 
 const NewCostForm = (props) => {
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
     const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
 
+    const titleFieldRef = useRef();
+    const amountFieldRef = useRef();
+    const dateFieldRef = useRef();
+
+    const isFormInvalid = () => [titleFieldRef, amountFieldRef, dateFieldRef]
+        .map((e) => e.current.reportValidity())
+        .some((e) => !!e);
+
     const titleChangeHandler = (e) => setTitle(e.target.value);
     const amountChangeHandler = (e) => setAmount(e.target.value);
     const dateChangeHandler = (e) => setDate(e.target.value);
-
-    const reportFormValidity = () => Array.from(document.querySelectorAll('[data-id="validation"]'))
-        .map((i) => i.reportValidity())
-        .every((i) => !!i);
     
     const clearForm = () => {
         setTitle('');
@@ -22,7 +28,9 @@ const NewCostForm = (props) => {
     }
     const submitHandler = (e) => {
         e.preventDefault();
-        if (!reportFormValidity()) return;
+        if (isFormInvalid()) {
+            return;
+        }
 
         const costData = {
             title,
@@ -44,38 +52,33 @@ const NewCostForm = (props) => {
         <Card className={styles['form-wrapper']}>
             <h2 className={styles['form-header']}>New Cost</h2>
             <form>
-                <div>
-                    <label>Title</label>
-                    <input 
-                        placeholder='Describe your expense...' 
-                        type='text' 
-                        value={title} 
-                        onChange={titleChangeHandler}
-                        data-id="validation"
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Amount</label>
-                    <input 
-                        type='number' 
-                        min='0.01' 
-                        step='0.01' 
-                        value={amount} 
-                        onChange={amountChangeHandler}
-                        data-id="validation"
-                    />
-                </div>
-                <div>
-                    <label>Cost Date</label>
-                    <input 
-                        type='date' 
-                        className='datepicker-input' 
-                        value={date} 
-                        onChange={dateChangeHandler}
-                        data-id="validation"
-                    />
-                </div>
+                <FormInput
+                    label='Title'
+                    placeholder='Describe your expense...' 
+                    type='text' 
+                    value={title} 
+                    onChange={titleChangeHandler}
+                    ref={titleFieldRef}
+                    required
+                />
+                <FormInput
+                    label='Amount'
+                    type='number'
+                    min='0.01' 
+                    step='0.01' 
+                    value={amount} 
+                    onChange={amountChangeHandler}
+                    ref={amountFieldRef}
+                    required
+                />
+                <FormInput
+                    label='Cost Date'
+                    type='date' 
+                    className='datepicker-input' 
+                    value={date} 
+                    onChange={dateChangeHandler}
+                    ref={dateFieldRef}
+                />
                 <div className={styles['buttons-block']}>
                     <button className={styles['submit-button']} onClick={submitHandler}>Confirm</button>
                     <button className={styles['cancel-button']} onClick={cancelHandler}>Cancel</button>
